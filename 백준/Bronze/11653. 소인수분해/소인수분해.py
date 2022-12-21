6e1e9fc40efe76575fdca0cc8e762f421eeca642
cyberman0306 <92566655+cyberman0306@users.noreply.github.com>
2022-12-21 20:33:30 +0900

    [Bronze I] Title: 소인수분해, Time: 956 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+while n != 1:
+    for i in range(2,n+1):
+        if n % i == 0:
+            n = n // i
+            print(i)
+            #print(n)
+            #print()
+            break
